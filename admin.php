3098981c1ed4d7ab1cb402c05f0aa384f6a2bbfb
renzivan14 <renzivanyapenguio@gmail.com>
2018-09-25 18:20:37 +0800

    New database

--- admin.php
+++ admin.php
@@ -125,10 +125,10 @@ if($getPrivilege==0){
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
-      <!-- Sidebar toggle button
+      <!-- Sidebar toggle button-->
       <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
         <span class="sr-only">Toggle navigation</span>
-      </a> -->
+      </a> 
 
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
